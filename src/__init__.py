8465afedf8761a85a180fb9eda83b646dfa6034b
zeroma25 <zeroma2505@yandex.ru>
2026-07-14 16:03:01 +0400

    chore: обновить версию до 0.4

--- src/__init__.py
+++ src/__init__.py
@@ -11,7 +11,7 @@ from .parser import RanobeLibParser
 from .processing import ContentProcessor
 from .settings import Settings, settings
 
-__version__ = "0.3"
+__version__ = "0.4"
 
 __all__ = [
     "ContentProcessor",
